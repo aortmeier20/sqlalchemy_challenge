288e8c60ab4037b15b1cbe253b04dc1d708c03fd
Austen <aortmeier20@gmail.com>
2024-05-27 21:06:26 -0500

    question 2 being worked out

--- climate_appAOEdit.py
+++ climate_appAOEdit.py
@@ -75,3 +75,20 @@ def precipitatin():
 
 ##3. Return a JSON list of stations from the dataset.
 
+@app.route("/api/v1.0/stations")
+def stations():
+        session = session(engine)
+
+        station_list = session.query(measurement.station).distinct().all()
+      
+        session.close()
+
+        station_data = []
+        for station in station_list:
+            station_dict = {}
+            station_dict["station name"] = station[0]
+            station_data.append(station_dict)
+        return jsonify(station_data)
+
+  
+        
