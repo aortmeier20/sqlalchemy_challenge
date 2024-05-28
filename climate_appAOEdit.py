20fd0fe350ab34f84e26b434c11c7a0e6f4c8c01
Austen <aortmeier20@gmail.com>
2024-05-27 19:52:26 -0500

    moved app setup to python

--- climate_appAOEdit.py
+++ climate_appAOEdit.py
@@ -1,9 +1,54 @@
+#climate app
+
+# Import the dependencies.
 import numpy as np
 import pandas as pd
 import datetime as dt
+
+
+#################################################
+# Database Setup
+#################################################
+
 # Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+base = automap_base()
+# reflect the tables
+base.prepare(autoload_with=engine)
+
+# Save references to each table
+measurement = base.classes.measurement
+station = base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+
+#flask setup
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return(f"Available Routes:<br/>"
+            f"/api/v1.0/precipitation<br/>"
+            f"/api/v1.0/stations<br/>"
+            f"/api/v1.0/tobs<br/>"
+            f"/api/v1.0/<start><br/>"
+            f"/api/v1.0/<start>/<end><br/>"
+            )
